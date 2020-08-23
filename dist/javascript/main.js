561f67399988bffb49a0bbaea0493017e788bdf8
jadenchant <jadenmchant@gmail.com>
2020-08-23 00:04:48 -0500

    idk with hover

--- dist/javascript/main.js
+++ dist/javascript/main.js
@@ -1,7 +1,57 @@
 const nav = document.querySelector('.nav');
 const navContents = document.querySelector('nav-c');
 const produce = document.querySelector('.nav-c-produce');
+const produceDrop = document.querySelector('.nav-c-produce-drop');
 const home = document.querySelector('.nav-c-home');
+const homeDrop = document.querySelector('.nav-c-home-drop');
 const clothing = document.querySelector('.nav-c-clothing');
+const clothingDrop = document.querySelector('.nav-c-clothing-drop');
 const tech = document.querySelector('.nav-c-tech');
-const navDropDown = document.querySelectorAll('');
+const techDrop = document.querySelector('.nav-c-tech-drop');
+
+const hoverColor = '#'
+
+// Produce
+produce.addEventListener('mouseenter', function(event) {
+    event.target.style.color = hoverColor;
+    produceDrop.style.visibility = 'visible';
+
+    setTimeout(function() {
+        event.target.style.color = '';
+        produceDrop.style.visibility = 'hidden';
+    }, 500);
+}, false);
+
+
+// Home
+home.addEventListener('mouseenter', function(event) {
+    event.target.style.color = hoverColor;
+    produceDrop.style.visibility = 'visible';
+
+    setTimeout(function() {
+        event.target.style.color = '';
+        produceDrop.style.visibility = 'hidden';
+    }, 500);
+}, false);
+
+// Clothing
+clothing.addEventListener('mouseenter', function(event) {
+    event.target.style.color = hoverColor;
+    produceDrop.style.visibility = 'visible';
+
+    setTimeout(function() {
+        event.target.style.color = '';
+        produceDrop.style.visibility = 'hidden';
+    }, 500);
+}, false);
+
+// Tech
+tech.addEventListener('mouseenter', function(event) {
+    event.target.style.color = hoverColor;
+    produceDrop.style.visibility = 'visible';
+
+    setTimeout(function() {
+        event.target.style.color = '';
+        produceDrop.style.visibility = 'hidden';
+    }, 500);
+}, false);
