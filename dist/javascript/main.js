f89456213a3610f5282f40a08bb7d54e8e88260b
jadenchant <jadenmchant@gmail.com>
2020-08-22 23:31:35 -0500

    Added more structure to index page

---
+++ dist/javascript/main.js
@@ -0,0 +1,7 @@
+const nav = document.querySelector('.nav');
+const navContents = document.querySelector('nav-c');
+const produce = document.querySelector('.nav-c-produce');
+const home = document.querySelector('.nav-c-home');
+const clothing = document.querySelector('.nav-c-clothing');
+const tech = document.querySelector('.nav-c-tech');
+const navDropDown = document.querySelectorAll('');
